5ae83cefaf7d3db2d1db85dad6e33ef90adc15ee
Scadoodle <105440207+School-Chromebook663@users.noreply.github.com>
2024-02-07 11:06:15 -0500

    hotfixes and stuff

--- assets/js/splash/main.js
+++ assets/js/splash/main.js
@@ -3,20 +3,19 @@ funky_title_splash = document.getElementById("funky-title-splash");
 	// Add Splash if Enabled
 if (localStorage.rv2_splashtext === "enabled") {
 
-		// Add Splash Resources
+		// First Time Entry
+	if (localStorage.rv2_seensplash === "no") {
+		funky_title_splash.innerHTML = "Inside Your Walls";
+	}
+
+		// Add Splash Text Directories
 	const splashArray = ["varDefault", "varMarch", "varOctober", "varNovember", "varDecember", "clickable"];
 	for (let i = 0; i < splashArray.length; ++i) {
 		let elem = splashArray[i];
 		onAddAsset("splash/" + elem, "script");
 	}
 
-		// Set Data if First Time
-	if (localStorage.rv2_seensplash === undefined) {
-		localStorage.rv2_seensplash = "yes";
-		funky_title_splash.innerHTML = "Inside Your Walls";
-	}
-
-		// Pick Random Text for the Index
+		// Call a Random Text Option
 	function onRandoText() {
 			// Seasonal (20% Chance)
 		if (Math.random() > 0.20) { // Default (80% Chance)
@@ -28,7 +27,7 @@ if (localStorage.rv2_splashtext === "enabled") {
 		}
 	};
 
-		// Seasonal Text
+		// Add Seasonal Text (if Called)
 	function onSeasonalSplash() {
 		var month = new Date().getMonth() + 1;
 		if (month == 3) { // March
@@ -50,16 +49,21 @@ if (localStorage.rv2_splashtext === "enabled") {
 		funky_title_splash.innerHTML = randomValue.splashEntry;
 	}
 
-		// Trigger Function on Index
+		// Set Default Values if None Present
+	if (localStorage.rv2_splashtext === undefined) {
+		localStorage.rv2_splashtext = "enabled";
+	}
+	if (localStorage.rv2_seensplash === undefined) {
+		localStorage.rv2_seensplash = "no";
+	}
+
+		// Trigger Randomized Text
 	window.onload = function() {
 		let funky_title_splash = document.getElementById("funky-title-splash");
-		if (typeof(funky_title_splash) != "undefined" && funky_title_splash != null) {
+		if (typeof(funky_title_splash) != "undefined" && funky_title_splash != null && localStorage.rv2_seensplash != "no") {
 			onRandoText();
+		} else { // First Time Entry
+			localStorage.rv2_seensplash = "yes";
 		}
 	}
-}
-
-	// Set Default Value if None Present
-if (localStorage.rv2_splashtext === undefined) {
-	localStorage.rv2_splashtext = "enabled";
 }
